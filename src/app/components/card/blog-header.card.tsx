3542600304d150a80155aca69742bc5bdc4d4667
mamun-sslwireless <abdullah.mamun@sslwireless.com>
2023-06-01 14:18:53 +0600

    blog header card added

---
+++ src/app/components/card/blog-header.card.tsx
@@ -0,0 +1,85 @@
+import { BsDot } from "react-icons/bs";
+
+type Category = {
+  id: number;
+  title: string;
+};
+
+type CreatedBy = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+type PropsTypes = {
+  id: number;
+  banner: string;
+  title: string;
+  category: Category;
+  text: string;
+  created_by: CreatedBy;
+  created_at: string;
+};
+
+export const BlogHeaderCard: React.FC<PropsTypes> = (
+  props: PropsTypes
+): JSX.Element => {
+  return (
+    <div className="overflow-hidden rounded-xl bg-white">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        {/* Banner */}
+        <div className="h-[400px] overflow-hidden">
+          <img
+            src="https://img.freepik.com/premium-vector/fifa-world-cup-championship-2022-background-football-banner-gradient-color_633084-155.jpg"
+            alt="..."
+            className="w-full min-h-[400px]"
+          />
+        </div>
+
+        {/* Content */}
+        <div className="p-4 lg:p-8">
+          <div className="mb-6">
+            <p className="text-md font-bold mb-3 text-gray-600">
+              {props.title}
+            </p>
+            <p className="text-sm leading-relaxed text-gray-500">
+              {props.text}
+            </p>
+          </div>
+
+          {/* Created by info */}
+          <div className="flex">
+            <div className="flex grow justify-start gap-2">
+              <div className="w-12 h-12 rounded-full overflow-hidden">
+                <img
+                  src={props.created_by.image}
+                  alt="..."
+                  className="max-w-12 max-h-12"
+                />
+              </div>
+              <div className="pt-[6px]">
+                <p className="text-[13px] font-medium leading-4">
+                  {props.created_by.name}
+                </p>
+                <div className="inline-flex text-gray-700">
+                  <p className="text-xs">{props.created_at}</p>
+                  <BsDot size={20} className="-mt-[2px]" />
+                  <p className="text-xs">{props.category.title}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex-none">
+              <button
+                type="button"
+                className="rounded-full px-3 py-2 text-sm font-bold transition-all text-gray-600 bg-gray-200 hover:bg-gray-300"
+              >
+                এনরোল করুন
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { FullWidthCarousel } from "@/app/components/carousel/full-width.carousel
 import { CourseCard } from "@/app/components/card/course.card";
 import { TeamCard } from "@/app/components/card/team.card";
 import { BlogCard } from "@/app/components/card/blog.card";
+import { BlogHeaderCard } from "@/app/components/card/blog-header.card";
 
 export default function Home() {
   return (
@@ -57,6 +58,8 @@ export default function Home() {
           </div>
         </ComponentBody>
 
+        {/* Blog list header card */}
+
         {/* Blog Card */}
         <ComponentBody
           title="Full width carousel slider"
@@ -81,6 +84,37 @@ export default function Home() {
               />
             ))}
           </div>
+        </ComponentBody>
+
+        {/* Blog header card */}
+        <ComponentBody
+          title="Blog header card"
+          description="Carousel component made with react-simply-carousel npm package."
+        >
+          <BlogHeaderCard
+            id={1}
+            banner="https://www.bristol.ac.uk/media-library/sites/engineering/computerscience/images/cs-research-module.jpg"
+            title="লরেম ইপ্সাম ডলর সিট আমেত, কনসেক্টেচুর অ্যাডিপিস্কিং এলিট। নানসি
+          আল্ট্রিচিস ভ্যারিয়াস আউগিউ ইউ ফ্যাউসিবাস।"
+            text={`লরেম ইপ্সাম ডলর সিট আমেত, কনসেক্টেচুর অ্যাডিপিস্কিং এলিট। নানসি
+              আল্ট্রিচিস ভ্যারিয়াস আউগিউ ইউ ফ্যাউসিবাস। ইন প্যালেনটেস্কিউ আউগিউ
+              নন নিকিউ টেম্পর ট্রিস্টিক। নিউলা এ পিউরাস অরনারে, ম্যাটুস এনিম
+              স্যাড, ম্যাক্সিমাস মি। ডোনেক পোর্টা রিউট্রাম ডিগনিসসিম। ডোনেক
+              ভাইটা ইরস এলিকুয়াম, রিউট্রাম রাইসাস এট, পরতিটর লরেম। আয়েনান স্যাড
+              এনিম লুকাস। সেড ইয়াকিউলাস এরাট আইডি ম্যাক্সিমাস হেন্ড্রেরিট।
+              নিউলাম এ এফিকিটার ইরস। স্যাড ইউ টিনসিডান্ট ফেলিস, এ অ্যাকিউমসান
+              এক্স। নাম ট্রিস্টিক ভলুটপাত নিসি এট অরনারে। ফিউস্ক ইন টার্পিস
+              কুয়াম। মরবি সাগিটিস ল্যাকটাস ইএস্টি, ভিটায়ে উলামকর্পার নিউলা
+              আলিকেট ভেল। ইন হ্যাক হ্যাবিটাসসে প্ল্যাটিয়া ডিস্কটামস্ট`}
+            category={{ id: 1, title: "Category" }}
+            created_by={{
+              id: 2,
+              name: "Captain Jack Sparrow",
+              image:
+                "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?w=60",
+            }}
+            created_at={"Jan 10, 2022"}
+          />
         </ComponentBody>
       </div>
     </main>
