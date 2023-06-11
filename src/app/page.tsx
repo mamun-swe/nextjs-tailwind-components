812cd992fdb5a47877a1dd9745e087c74fa0b358
mamun-sslwireless <abdullah.mamun@sslwireless.com>
2023-06-11 17:47:47 +0600

    group carousel added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { HeaderCarousel } from "@/app/components/carousel/header.carousel";
 import { TrainingCarousel } from "@/app/components/carousel/training.carousel";
+import { GroupCarousel } from "@/app/components/carousel/group.carousel";
 
 export default function Home() {
   return (
     <main className="py-4">
-      <div className="container">
-        {/* <HeaderCarousel /> */}
-      </div>
+      <div className="container">{/* <HeaderCarousel /> */}</div>
 
       <br />
       <br />
       <br />
 
-      <TrainingCarousel />
+      {/* <TrainingCarousel /> */}
+      <GroupCarousel />
     </main>
   );
 }
