3905c6280c437cde628623a730e95774ce22a2c7
mamun-sslwireless <abdullah.mamun@sslwireless.com>
2023-06-11 12:21:16 +0600

    full width carousel slider added

--- src/app/components/carousel/full-width.carousel.tsx
+++ src/app/components/carousel/full-width.carousel.tsx
@@ -1,9 +1,33 @@
 "use client";
+// var Carousel = require("react-responsive-carousel").Carousel;
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export const FullWidthCarousel: React.FC = (): JSX.Element => {
+  const styleClass = "h-[240px] md:h-[500px]";
   return (
-    <div>
-      <p>Carousel slider incoming...</p>
+    <div className="h-[500px] overflow-hidden">
+      <Carousel
+        showArrows={true}
+        showStatus={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        animationHandler={"fade"}
+
+        // onChange={onChange}
+        // onClickItem={onClickItem}
+        // onClickThumb={onClickThumb}
+      >
+        <div className={styleClass}>
+          <img src="1.jpg" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div className={styleClass}>
+          <img src="2.jpg" />
+          <p className="legend">Legend 1</p>
+        </div>
+      </Carousel>
     </div>
   );
 };
